03e518652b52adcc50d298c42e35e913c5ad1cf9
smartankur4u <ankurnarkhede@gmail.com>
2018-03-17 00:56:59 +0530

    url model added

--- shorten/models.py
+++ shorten/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class url(models.Model):
+    long_url=models.CharField(max_length=9999)
+    short_hash=models.CharField(max_length=9999)
+    count=models.IntegerField()
+    status=models.BooleanField()
+    created_at=models.DateTimeField()
+
+
+    def __str__(self):
+        return self.short_hash
