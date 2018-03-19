f4026e576afc4868e5a20f0a24958998e5212dd8
smartankur4u <ankurnarkhede@gmail.com>
2018-03-19 23:39:20 +0530

    ...some changes

--- shorten/views.py
+++ shorten/views.py
@@ -6,22 +6,24 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import url
 from .serializers import url_serializer
-
-
-from .models import url
-from rest_framework import viewsets
-from .serializers import url_serializer
+from django.http import JsonResponse
 
 
 
 class url_list(APIView):
-
     def get(self, request):
-        # urls=url.objects.get(short_hash="qwertyui")
-        lol=request.path
-        urls = url.objects.all()
-        serializer=url_serializer(urls, many=True)
-        return Response(serializer.data)
+        print(self.request.query_params.get('short_hash'), self.request.query_params)
+        # shorted = self.request.GET.get('short_hash') # Ek min thamb. mala search karun pahu dekkkk
+        shorted = self.request.query_params.get('short_hash')
+        try:
+            req_url=url.objects.get(short_hash=shorted)
+            req_url.count = req_url.count + 1
+            req_url.save()
+            serializer=url_serializer(req_url)
+            return Response(serializer.data)
+        except Exception as e:
+            print(e)
+            return JsonResponse({"mesage": "Requested URL not dfound"})
 
     def post(self, request):
         long_url = request.POST.get('long_url', None)
@@ -39,3 +41,5 @@ class url_list(APIView):
         return Response (serializer.data)
 
 
+
+# # def post kam karat ahe ka?...yuppp hoooooooo He run kuthun hotay. Tithe te error print kelay...te get nahi hotey param...me pan try kela hota pahile yane
